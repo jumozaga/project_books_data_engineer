1a409971d2be839e801cf4c646d360fc36cfb35c
jumozaga <jumozaga@yahoo.com>
2023-04-17 14:06:51 -0300

    Script finished - Category + Pages

--- app_soup3.py
+++ app_soup3.py
@@ -1,4 +1,3 @@
-
 import os
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
@@ -12,61 +11,87 @@ host = os.getenv("HOST")
 port = os.getenv("PORT")
 db_name = os.getenv("DATABASE")
 
-db = conect_db(con,host, port, db_name)
-    
-
+db = conect_db(con, host, port, db_name)
 
 # URL do site que será feito o web scraping
-url = "http://books.toscrape.com/index.html"
+url = "http://books.toscrape.com/"
 
 # Realiza a requisição HTTP para a página
 response = requests.get(url)
 
 # Cria um objeto BeautifulSoup com o conteúdo HTML da página
 soup = BeautifulSoup(response.text, "html.parser")
-nav = soup.select_one(".nav-list")
-categorias = [a.text.strip() for a in nav.select("a")]
-# livros = soup.select(".product_pod")
+
+categorias = soup.find('ul', {'class': 'nav-list'}).find('ul').find_all(
+    'a') if soup.find('ul', {'class': 'nav-list'}) else None
+
 for categoria in categorias:
-    print(f"Processando categoria: {categoria}")
-    url2 = f"https://books.toscrape.com/catalogue/category/books/{categoria.lower().replace(' ', '-')}/index.html"
-    print(url2)
+    
+    print(f"Processando categoria: {categoria.text.strip()}")
+
+    # MONTA A URL COM A CATEGORIA 
+    url2 = url + categoria['href']
+
+    # USA NOVO ENDEREÇO COM A CATEGORIA  
     response2 = requests.get(url2)
     html = response2.text
     soup2 = BeautifulSoup(html, "html.parser")
-    print(soup2)
-    # Processar a página e extrair a categoria de cada livro    
-    livros = soup.select('[class=\"col-xs-6 col-sm-4 col-md-3 col-lg-3\"]')
-    print(livros)
-    
-    print("Fim da Categoria:'{}'".format(categoria))
-    for livro in livros:
+
+    # ENCONTRA A QUANTIDADE DE PAGINAS / SESSÃO
+    current = soup2.find("li", class_="current")
+    current = int(current.text.split("of")[1]) if current else 1
+
+    # VARRE AS PAGINAS DENTRO DA URL CATEGORIA
+    for n in range(1, current+1):
+        pages = url2.replace("index.html", f"page-{n}.html") if current > 1 else url2
+        response3 = requests.get(pages)
+        html3 = response3.text
+        soup3 = BeautifulSoup(html3, "html.parser")
+
+        # PEGA OS LIVROS NA PAGINA DA CATEGORIA NA PAG X
+        livros = soup3.find_all('article', class_='product_pod')
+
+        for livro in livros:
+
             titulo = livro.select_one("h3 a").text
+
             preco = livro.select_one(".price_color").text
+
             estrelas = livro.select_one("p.star-rating")["class"][-1]
-            categoria = categoria
-            # categoria = soup2.select_one(".breadcrumb li:nth-of-type(3) a")
+
+            categoria_nome = categoria.text.strip()
+
             estoque = livro.select_one(".instock.availability").text.strip()
 
-
             # Cria um dicionário com os dados do livro
-            print("Criando Dicionario")
-
             livro_data = {
                 "titulo": titulo,
-                "categoria": categoria,
+                "categoria": categoria_nome,
                 "estrelas": estrelas,
                 "preco": preco,
                 "estoque": estoque,
             }
-            print("Dicionario criado")
 
-            print("Criando CSV em UTF-8")
+            # Insere o livro no arquivo CSV
             with open("livros3.csv", "a", encoding="utf-8") as f:
-                f.write(f"{titulo},{categoria},{estrelas},{preco},{estoque} \n")
+                f.write(f"{titulo},{categoria_nome},{estrelas},{preco},{estoque} \n")
             print("CSV criado")
-            
+
             # Insere o livro no banco de dados MongoDB
-            print("Indo lá no BD, me deseja boa sorte hehe")
+            print("SALVANDO OS DADOS NO BANCO DE DADOS")
             db.livros.insert_one(livro_data)
-            print("Inserido no BD")
+
+print("DADOS SALVOS COM SUCESSO")
+
+
+
+       # nav = soup.select_one(".nav-list")
+        # categorias = [a.text.strip() for a in nav.select("a")]
+
+    # url2 = f"""https://books.toscrape.com/catalogue/category/books/{
+    #     categoria.lower()+ "_"}/index.html
+    # """
+    
+
+        # Processar a página e extrair a categoria de cada livro
+        # livros = soup3.select('[class="col-xs-6 col-sm-4 col-md-3 col-lg-3"]')
