c179eaf2ee42e952139337702770c38bbfe4886f
jumozaga <jumozaga@yahoo.com>
2023-04-17 23:23:50 -0300

    test csv

--- app_soup3.py
+++ app_soup3.py
@@ -22,17 +22,19 @@ response = requests.get(url)
 # Cria um objeto BeautifulSoup com o conteúdo HTML da página
 soup = BeautifulSoup(response.text, "html.parser")
 
-categorias = soup.find('ul', {'class': 'nav-list'}).find('ul').find_all(
-    'a') if soup.find('ul', {'class': 'nav-list'}) else None
+categorias = (
+    soup.find("ul", {"class": "nav-list"}).find("ul").find_all("a")
+    if soup.find("ul", {"class": "nav-list"})
+    else None
+)
 
 for categoria in categorias:
-    
     print(f"Processando categoria: {categoria.text.strip()}")
 
-    # MONTA A URL COM A CATEGORIA 
-    url2 = url + categoria['href']
+    # MONTA A URL COM A CATEGORIA
+    url2 = url + categoria["href"]
 
-    # USA NOVO ENDEREÇO COM A CATEGORIA  
+    # USA NOVO ENDEREÇO COM A CATEGORIA
     response2 = requests.get(url2)
     html = response2.text
     soup2 = BeautifulSoup(html, "html.parser")
@@ -42,23 +44,23 @@ for categoria in categorias:
     current = int(current.text.split("of")[1]) if current else 1
 
     # VARRE AS PAGINAS DENTRO DA URL CATEGORIA
-    for n in range(1, current+1):
+    for n in range(1, current + 1):
         pages = url2.replace("index.html", f"page-{n}.html") if current > 1 else url2
         response3 = requests.get(pages)
         html3 = response3.text
         soup3 = BeautifulSoup(html3, "html.parser")
 
         # PEGA OS LIVROS NA PAGINA DA CATEGORIA NA PAG X
-        livros = soup3.find_all('article', class_='product_pod')
+        livros = soup3.find_all("article", class_="product_pod")
 
         for livro in livros:
-
             # titulo = livro.select_one("h3 a").text
-            titulo = livro.h3.a['title']
+            titulo = livro.h3.a["title"].replace(',', '.').strip()
 
-            preco = livro.select_one(".price_color").text
+            preco = livro.select_one(".price_color").text.strip()
+            # preco = livro.select("p")[1].text.strip()
 
-            estrelas = livro.select_one("p.star-rating")["class"][-1]
+            estrelas = livro.select_one("p.star-rating")["class"][-1].strip()
 
             categoria_nome = categoria.text.strip()
 
@@ -75,7 +77,10 @@ for categoria in categorias:
 
             # Insere o livro no arquivo CSV
             with open("livros3.csv", "a", encoding="utf-8") as f:
-                f.write(f"{titulo},{categoria_nome},{estrelas},{preco},{estoque} \n")
+                f.write(
+                    f"{titulo},{categoria_nome},{estrelas},\
+                        {preco},{estoque} \n"
+                )
             print("CSV criado")
 
             # Insere o livro no banco de dados MongoDB
@@ -85,14 +90,13 @@ for categoria in categorias:
 print("DADOS SALVOS COM SUCESSO")
 
 
+# nav = soup.select_one(".nav-list")
+# categorias = [a.text.strip() for a in nav.select("a")]
 
-       # nav = soup.select_one(".nav-list")
-        # categorias = [a.text.strip() for a in nav.select("a")]
+# url2 = f"""https://books.toscrape.com/catalogue/category/books/{
+#     categoria.lower()+ "_"}/index.html
+# """
 
-    # url2 = f"""https://books.toscrape.com/catalogue/category/books/{
-    #     categoria.lower()+ "_"}/index.html
-    # """
-    
 
-        # Processar a página e extrair a categoria de cada livro
-        # livros = soup3.select('[class="col-xs-6 col-sm-4 col-md-3 col-lg-3"]')
+# Processar a página e extrair a categoria de cada livro
+# livros = soup3.select('[class="col-xs-6 col-sm-4 col-md-3 col-lg-3"]')
